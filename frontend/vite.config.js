887a02af68e8f3de79a51b3f867e6b87f9721a86
thedevanshagrawal <138374457+thedevanshagrawal@users.noreply.github.com>
2024-08-16 10:16:55 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/users': 'http://localhost:3000/api/v1',
+      // '/users': 'http://localhost:3000/api/v1',
       '/users': 'https://exam-portal-backend-wvpw.onrender.com/api/v1'
     }
   },
